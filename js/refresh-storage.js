5f01976ccc24487bdbf6a4ab29c88e24b9a47f5b
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-16 11:33:53 +0200

    Refresh storage example 2

--- js/refresh-storage.js
+++ js/refresh-storage.js
@@ -1,16 +1,12 @@
 let isCleared = window.localStorage.getItem('isCleared');
 if (isCleared != "true") {
-  let keysToDelete = ['val1', 'val2', 'val3'];
+  let lang = window.localStorage.getItem('lang');
+  let sound = window.localStorage.getItem('sound');
 
-  for (let i = 0; i < window.localStorage.length;) {
-    let key = window.localStorage.key(i);
+  window.localStorage.clear();
 
-    if (keysToDelete.includes(key)) {
-      window.localStorage.removeItem(key);
-    } else {
-      i++;
-    }
-  }
+  window.localStorage.setItem('lang', lang);
+  window.localStorage.setItem('sound', sound);
 
   window.localStorage.setItem('isCleared', true);
 }
